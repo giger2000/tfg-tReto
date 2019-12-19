35209f6d774f9982a0911866cc300938ae684cd5
giger2000 <albert.corral@gmail.com>
2019-12-20 00:54:23 +0100

    normas con loader
    Página de normas presenta loader sencillo y mensaje de no hay registros

--- src/app/pages/normas/normas.page.ts
+++ src/app/pages/normas/normas.page.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 import { NormaModel } from './../../interface/norma.model';
+import { NormasService } from './../../services/normas.service';
 
 // Firebase
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
-//Ionic loading
+// Ionic loading
 import { LoadingController } from '@ionic/angular';
 
 @Component({
@@ -16,21 +17,34 @@ import { LoadingController } from '@ionic/angular';
 })
 export class NormasPage implements OnInit {
 
+  // Para los cargadores de alertas iniciales
+  cargando = false;
+
+  tempNormas: NormaModel[] = []; //Para el loading
+
 
   $normas: Observable<NormaModel[]>; // porque el array varía en tiempo real
   normasCollection: AngularFirestoreCollection<NormaModel>;
 
   constructor(
     private db: AngularFirestore,
-    public loadingController: LoadingController
-  ) { }
+    public loadingController: LoadingController,
+    private normasService: NormasService
+  ) {
+  }
 
   ngOnInit() {
-
+    this.cargando = true;
     this.normasCollection = this.db.collection<NormaModel>('norma');
     this.$normas = this.normasCollection.valueChanges();
+  
+    this.normasService.getNormas()
+    .subscribe ( resp => {
+            this.tempNormas = resp;
+            this.cargando = false;
+            console.log('respuesta: ', resp);
+    });
 
-    console.log(this.$normas);
   }
 
   // LOADER
--- src/app/services/normas.service.ts
+++ src/app/services/normas.service.ts
@@ -6,7 +6,6 @@ import { Observable } from 'rxjs';
 // import { map } from 'rxjs/operators';
 import { NormaModel } from '../interface/norma.model';
 
-import { NgForm } from '@angular/forms';
 
 
 @Injectable({
@@ -16,27 +15,45 @@ export class NormasService {
 
   normas: Observable<NormaModel[]>;
   normasCollection: AngularFirestoreCollection<NormaModel>;
-  
-  
 
   constructor(
     private db: AngularFirestore
     ) {
-      this.normasCollection = this.db.collection<NormaModel>('norma');
-      this.normas = this.normasCollection.valueChanges();
+      // this.normasCollection = this.db.collection<NormaModel>('norma');
+      // this.normas = this.normasCollection.valueChanges();
     }
 
-    private addNorma(nombre: string, deporte: string, criterio: string, minValor: number, coeficiente: number) { 
-      const id = this.db.createId();
-      const newNorma: NormaModel = {
-        nombre,
-        deporte,
-        criterio,
-        minValor,
-        coeficiente
-  
-    };
-    this.normasCollection.doc(id).set(newNorma);
+  //   private addNorma(nombre: string, deporte: string, criterio: string, minValor: number, coeficiente: number) { 
+  //     const id = this.db.createId();
+  //     const newNorma: NormaModel = {
+  //       nombre,
+  //       deporte,
+  //       criterio,
+  //       minValor,
+  //       coeficiente
+  //  };
+  //   this.normasCollection.doc(id).set(newNorma);
 
+  // }
+
+  getNormas() {
+
+    // var docRef = this.db.collection<NormaModel>('norma').doc('nr1');
+
+    // docRef.get().then(function(doc) {
+    //     if (doc.exists) {
+    //         console.log("Document data:", doc.data());
+    //         return doc;
+    //     } else {
+    //         // doc.data() will be undefined in this case
+    //         console.log("No such document!");
+    //     }
+    // }).catch(function(error) {
+    //     console.log("Error getting document:", error);
+    // });
+
+    this.normasCollection = this.db.collection<NormaModel>('norma');
+    this.normas = this.normasCollection.valueChanges();
+    return this.normas;
   }
 }
